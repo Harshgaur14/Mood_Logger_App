86ab3882c405e0e500b3aad4093f114e5594b69a
gaurharsh14 <gaur.harsh007@yahoo.com>
2025-09-16 19:47:27 +0530

    setup

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to MoodLogger!');
+});
+
+app.listen(port, () => {
+    console.log(`MoodLogger app running at http://localhost:${port}`);
+});
